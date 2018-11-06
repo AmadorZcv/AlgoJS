d60ee999d95a542dea6caa10c2dd387d182734a1
Lucas Amador de Oliveira <lucasamadorzcv@gmail.com>
2018-11-06 20:42:37 -0200

    Calculo de horas de séries

--- time/main.js
+++ time/main.js
@@ -1,46 +1,9 @@
-function calculateDuraçãoSérie(qtd, duraçãoMin) {
-    return qtd * duraçãoMin;
-}
-function horaParaMinuto(hora) {
-    return hora * 60;
-}
-function minutosToHora(minutos) {
-    const horasInteiras = Math.trunc(minutos);
-    const horaQuebrada = (minutos - horasInteiras * 60) / 60;
-    return horasInteiras + horaQuebrada;
-}
 
-function horaParaDias(horas) {
-    const horasNew = Math.trunc(horas / 24);
-    const minutos = horaParaMinuto((horas / 24) - horasNew);
-    return horas + minutosToHora(minutos);
-}
-function diasParaHora(dias) {
-    return dias * 24;
-}
-function horasExcedentesParaHoraFinal(numHoras, horaInicio) {
-    const horas = horaInicio + numHoras;
-    if (horas > 24) {
-        return { dias: 1, totalHoras: horas - 24 }
-    }
-    return { dias: 0, totalHoras: horas };
-}
-function calculateHoraDeFim(qtd, duraçãoMin, horaInicio) {
-    const duraçãoHora = minutosToHora(calculateDuraçãoSérie(qtd, duraçãoMin));
-    const horaInicioSPlit = parseInt(horaInicio.split(":")[0]);
-    const minutosInicioSplit = parseInt(horaInicio.split(":")[1]);
-    const horasInicio = horaInicioSPlit + minutosToHora(minutosInicioSplit);
-    console.log("HorasInicio é", horasInicio);
-    //////////////////////
-    const duraçãoDias = horaParaDias(duraçãoHora + horasInicio);
-    let numDias = Math.trunc(duraçãoDias);
-    const numHoras = diasParaHora(duraçãoDias - numDias);
-    console.log("DUraçãoHora é", duraçãoDias);
-    console.log("Num dias é",numDias);
-    const { dias, totalHoras } = horasExcedentesParaHoraFinal(numHoras, horasInicio);
-    numDias = numDias + dias;
-    const horaFinalString = `${Math.trunc(totalHoras)}:${horaParaMinuto(totalHoras - Math.trunc(totalHoras))}`
-    console.log("Hora final é", horaFinalString)
+function calculateHoraDeFim(qtd, duraçãoHora) {
+    const duração = qtd * duraçãoHora;
+    const restoHoras = duração%24;
+    const dias = Math.trunc(duração/24);
+    return {dias,horaFinal:restoHoras}
 }
 //Valores dos inputs base
 let episodio = 0;
@@ -53,7 +16,19 @@ episodioDoc.value = episodio;
 let qtdDoc = document.getElementById("qtd");
 qtdDoc.value = qtd;
 //Documentos do horario
-let horarioDoc = document.getElementById("horario");
-horarioDoc.value = horario;
+let resultadoDoc = document.getElementById("resultado");
+function onEpisodioChange() {
+    const duração = episodioDoc.value;
+    const qtd =qtdDoc.value;
+    const{dias,horaFinal}=calculateHoraDeFim(qtd,duração);
+    resultadoDoc.value=`Se passaram ${dias} dias e a hora final vai ser ${horaFinal}`
+}
+function onQtdChange() {
+    const duração = episodioDoc.value;
+    const qtd =qtdDoc.value;
+    const{dias,horaFinal}=calculateHoraDeFim(qtd,duração);
+    resultadoDoc.value=`Se passaram ${dias} dias e a hora final vai ser ${horaFinal} horas depois do inicio`
+}
+
 
 
